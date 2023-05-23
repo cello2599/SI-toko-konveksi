50879b24855e72518bef7d68c5eeb74671cffeae
Wawan07364 <wawanstams123@gmail.com>
2023-05-23 19:46:32 +0700

    simulasi

--- app/Http/Controllers/ProdukController.php
+++ app/Http/Controllers/ProdukController.php
@@ -24,7 +24,7 @@ class ProdukController extends Controller
         return ProdukResource::collection($produk);
     }
 
-    //input data ke database
+    //how to insert data to database
     public function store(Request $request)
     {   
     
